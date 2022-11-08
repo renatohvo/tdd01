8a5b8a3071c87de2543edd3f0d811ddf1fe85bf2
renatohvo <renatohvo@gmail.com>
2022-11-07 21:53:34 -0300

    Employee-Insert

--- backend/src/main/java/com/renatohvo/tdd01/controllers/EmployeeController.java
+++ backend/src/main/java/com/renatohvo/tdd01/controllers/EmployeeController.java
@@ -1,5 +1,7 @@
 package com.renatohvo.tdd01.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -7,8 +9,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.renatohvo.tdd01.dto.EmployeeDTO;
 import com.renatohvo.tdd01.services.EmployeeService;
@@ -27,5 +32,12 @@ public class EmployeeController {
 		Page<EmployeeDTO> list = service.findAllPaged(pageRequest);
 		return ResponseEntity.ok().body(list);
 	}
-
+	
+	@PostMapping
+	public ResponseEntity<EmployeeDTO> insert(@RequestBody EmployeeDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
 }
--- backend/src/main/java/com/renatohvo/tdd01/services/EmployeeService.java
+++ backend/src/main/java/com/renatohvo/tdd01/services/EmployeeService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.renatohvo.tdd01.dto.EmployeeDTO;
+import com.renatohvo.tdd01.entities.Department;
 import com.renatohvo.tdd01.entities.Employee;
 import com.renatohvo.tdd01.repositories.EmployeeRepository;
 
@@ -21,4 +22,19 @@ public class EmployeeService {
 		Page<Employee> list = repository.findAll(pageable);
 		return list.map(entity -> new EmployeeDTO(entity));
 	}
+
+	@Transactional
+	public EmployeeDTO insert(EmployeeDTO dto) {
+		Employee entity = new Employee();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new EmployeeDTO(entity);
+	}
+	
+	private void copyDtoToEntity(EmployeeDTO dto, Employee entity) {
+		entity.setName(dto.getName());
+		entity.setEmail(dto.getEmail());
+		entity.setDepartment(new Department(dto.getDepartmentId(), null));
+	}
+	
 }
